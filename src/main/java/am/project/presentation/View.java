49b6da6c84651fe47a5330358ec962e4e4aadc28
Valeria Gutiérrez Mata <valeria.gutierrez.mata@est.una.ac.cr>
2023-05-29 20:52:06 -0600

    Front end nuevo

--- src/main/java/am/project/presentation/View.java
+++ src/main/java/am/project/presentation/View.java
@@ -5,6 +5,13 @@ import javax.swing.*;
 public class View {
     private JPanel panelPrincipal;
     private JLabel mainLabel;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JButton AgregarButton;
+    private JTextField NombreTextField;
+    private JTextField DuracionTextField;
+    private JTextField LlegadaTextField;
+    private JTextField PrioridadTextField;
 
     public View() {
     }
